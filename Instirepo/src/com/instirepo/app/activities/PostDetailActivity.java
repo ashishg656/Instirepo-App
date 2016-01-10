8afcbf6ee1b65d51eb7399c627aec6859746f86c
ashish <ashishg656@gmail.com>
2016-01-10 23:48:45 +0530

    changed post detail activity

--- Instirepo/src/com/instirepo/app/activities/PostDetailActivity.java
+++ Instirepo/src/com/instirepo/app/activities/PostDetailActivity.java
@@ -2,17 +2,31 @@ package com.instirepo.app.activities;
 
 import com.instirepo.app.R;
 import com.instirepo.app.objects.PostListSinglePostObject;
+import com.instirepo.app.widgets.CircularImageView;
+import com.instirepo.app.widgets.ObservableScrollView;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PostDetailActivity extends BaseActivity {
 
 	PostListSinglePostObject postListSinglePostObject;
+	ImageView postImage;
+	TextView postHeading, postDescription, category, uploaderName, uploadTime,
+			numberOfPeopleViewed, votes, comments, viewSeensButton, followPost,
+			reportPost;
+	CircularImageView uploaderImage;
+	RecyclerView seenByRecyclerView;
+	ObservableScrollView scrollView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.post_detail_activity_layout);
+
+		postImage = (ImageView) findViewById(R.id.postimage);
 
 		if (getIntent().hasExtra("postobj")) {
 			postListSinglePostObject = getIntent().getExtras().getParcelable(
@@ -22,7 +36,7 @@ public class PostDetailActivity extends BaseActivity {
 	}
 
 	private void setInitialDataUsingnIntentObj() {
-		
+
 	}
 
 }
