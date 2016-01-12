5722c14bc6fa53006235bd470891490b67697e79
ashish <ashishg656@gmail.com>
2016-01-12 17:41:49 +0530

    made buttons working in PDP

--- Instirepo/src/com/instirepo/app/activities/PostDetailActivity.java
+++ Instirepo/src/com/instirepo/app/activities/PostDetailActivity.java
@@ -1,24 +1,44 @@
 package com.instirepo.app.activities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request.Method;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.instirepo.app.R;
 import com.instirepo.app.application.ZApplication;
+import com.instirepo.app.extras.AppConstants;
 import com.instirepo.app.extras.TimeUtils;
+import com.instirepo.app.extras.ZUrls;
+import com.instirepo.app.fragments.CommentsFragment;
+import com.instirepo.app.fragments.SeenByPeopleFragment;
+import com.instirepo.app.fragments.UserProfileViewedByOtherFragment;
 import com.instirepo.app.objects.PostListSinglePostObject;
+import com.instirepo.app.preferences.ZPreferences;
 import com.instirepo.app.serverApi.ImageRequestManager;
 import com.instirepo.app.widgets.CircularImageView;
 import com.instirepo.app.widgets.ObservableScrollView;
 
-public class PostDetailActivity extends BaseActivity {
+public class PostDetailActivity extends BaseActivity implements AppConstants,
+		OnClickListener, ZUrls {
 
 	PostListSinglePostObject postListSinglePostObject;
-	ImageView postImage;
+	ImageView postImage, reportPostImage, followPostImage, savePostImage,
+			upvoteImage, downvoteImage;
 	TextView postHeading, postDescription, category, uploaderName, uploadTime,
 			numberOfPeopleViewed, votes, comments, viewSeensButton, followPost,
 			reportPost;
@@ -26,10 +46,15 @@ public class PostDetailActivity extends BaseActivity {
 	RecyclerView seenByRecyclerView;
 	ObservableScrollView scrollView;
 
+	boolean previouslyUpvoted, previouslyDownvoted, previousUpvotes,
+			previousDownvotes;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.post_detail_activity_layout);
+
+		setProgressLayoutVariablesAndErrorVariables();
 
 		postImage = (ImageView) findViewById(R.id.postimage);
 		postHeading = (TextView) findViewById(R.id.postHeading);
@@ -44,16 +69,23 @@ public class PostDetailActivity extends BaseActivity {
 		followPost = (TextView) findViewById(R.id.followposttext);
 		reportPost = (TextView) findViewById(R.id.reportposttext);
 		uploaderImage = (CircularImageView) findViewById(R.id.circularimage);
+		reportPostImage = (ImageView) findViewById(R.id.rportposticoon);
+		followPostImage = (ImageView) findViewById(R.id.followbuttonimage);
+		savePostImage = (ImageView) findViewById(R.id.savepostbutton);
+		upvoteImage = (ImageView) findViewById(R.id.upvotepostimage);
+		downvoteImage = (ImageView) findViewById(R.id.downvotepostimage);
+
+		findViewById(R.id.openuserprofilepost).setOnClickListener(this);
+		findViewById(R.id.seenbycontainer).setOnClickListener(this);
+		findViewById(R.id.commnts).setOnClickListener(this);
+		upvoteImage.setOnClickListener(this);
+		downvoteImage.setOnClickListener(this);
+		savePostImage.setOnClickListener(this);
 
 		if (getIntent().hasExtra("postobj")) {
 			postListSinglePostObject = getIntent().getExtras().getParcelable(
 					"postobj");
-			setInitialDataUsingnIntentObj();
-		}
-	}
 
-	private void setInitialDataUsingnIntentObj() {
-		if (postListSinglePostObject != null) {
 			if (postListSinglePostObject.getImage() != null) {
 				ImageRequestManager.get(this).requestImage(
 						this,
@@ -62,6 +94,18 @@ public class PostDetailActivity extends BaseActivity {
 								.getImage()), -1);
 			}
 
+			ImageRequestManager.get(this).requestImage(this, uploaderImage,
+					postListSinglePostObject.getUser_image(), -1);
+
+			setInitialDataUsingnIntentObj();
+		}
+	}
+
+	private void setInitialDataUsingnIntentObj() {
+		hideErrorLayout();
+		hideLoadingLayout();
+
+		if (postListSinglePostObject != null) {
 			postHeading.setText(postListSinglePostObject.getHeading());
 			postDescription.setText(postListSinglePostObject.getDescription());
 			category.setText(postListSinglePostObject.getCategory());
@@ -78,16 +122,27 @@ public class PostDetailActivity extends BaseActivity {
 			if (postListSinglePostObject.isIs_following()) {
 				followPost.setText(getResources().getString(
 						R.string.following_post));
-			} else
+				followPostImage.setSelected(true);
+			} else {
 				followPost.setText(getResources().getString(
 						R.string.follow_post));
-			
-			if (postListSinglePostObject.isIs_reported())
+				followPostImage.setSelected(false);
+			}
+
+			if (postListSinglePostObject.isIs_reported()) {
 				reportPost.setText(getResources().getString(
 						R.string.reported_post));
-			else
+				reportPostImage.setSelected(true);
+			} else {
 				reportPost.setText(getResources().getString(
 						R.string.report_post));
+				reportPostImage.setSelected(false);
+			}
+
+			savePostImage.setSelected(postListSinglePostObject.isIs_saved());
+			upvoteImage.setSelected(postListSinglePostObject.isHas_upvoted());
+			downvoteImage.setSelected(postListSinglePostObject
+					.isHas_downvoted());
 
 			GradientDrawable categoryBg = (GradientDrawable) category
 					.getBackground();
@@ -97,10 +152,230 @@ public class PostDetailActivity extends BaseActivity {
 					getResources().getDimensionPixelSize(R.dimen.z_one_dp),
 					color);
 			category.setTextColor(color);
-
-			ImageRequestManager.get(this).requestImage(this, uploaderImage,
-					postListSinglePostObject.getUser_image(), -1);
 		}
 	}
 
+	public void upvoteOrDownvotePost(boolean isUpvoteClicked) {
+		if (postListSinglePostObject.isHas_upvoted()) {
+			// upvoted
+			if (isUpvoteClicked) {
+				showSnackBar("Already Upvoted");
+				return;
+			} else {
+				showSnackBar("Neither upvoted nor downvoted");
+				postListSinglePostObject.setHas_upvoted(false);
+				postListSinglePostObject.setUpvotes(postListSinglePostObject
+						.getUpvotes() - 1);
+				setInitialDataUsingnIntentObj();
+			}
+		} else if (postListSinglePostObject.isHas_downvoted()) {
+			// downvoted
+			if (isUpvoteClicked) {
+				showSnackBar("Neither upvoted nor downvoted");
+				postListSinglePostObject.setHas_downvoted(false);
+				postListSinglePostObject.setDownvotes(postListSinglePostObject
+						.getDownvotes() - 1);
+				setInitialDataUsingnIntentObj();
+			} else {
+				showSnackBar("Already Downvoted");
+				return;
+			}
+		} else {
+			// neither upvoted nor downvoted
+			if (isUpvoteClicked) {
+				showSnackBar("Upvoted");
+				postListSinglePostObject.setHas_upvoted(true);
+				postListSinglePostObject.setUpvotes(postListSinglePostObject
+						.getUpvotes() + 1);
+				setInitialDataUsingnIntentObj();
+			} else {
+				showSnackBar("Downvoted");
+				postListSinglePostObject.setHas_downvoted(true);
+				postListSinglePostObject.setDownvotes(postListSinglePostObject
+						.getDownvotes() + 1);
+				setInitialDataUsingnIntentObj();
+			}
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.openuserprofilepost:
+			switchToUserProfileViewedByOtherFragment(
+					postListSinglePostObject.getUser_id(),
+					postListSinglePostObject.getUser_name(),
+					postListSinglePostObject.getUser_image());
+			break;
+		case R.id.seenbycontainer:
+			switchToSeenByPeopleFragment(postListSinglePostObject.getId());
+			break;
+		case R.id.commnts:
+			switchToCommentsFragment(postListSinglePostObject.getId());
+			break;
+		case R.id.savepostbutton:
+			savePostAsImportant();
+			break;
+		case R.id.upvotepostimage:
+			upvoteOrDownvotePost(true);
+			break;
+		case R.id.downvotepostimage:
+			upvoteOrDownvotePost(false);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private void savePostAsImportant() {
+		if (postListSinglePostObject.isIs_saved()) {
+			postListSinglePostObject.setIs_saved(false);
+			postListSinglePostObject.setSaves(postListSinglePostObject
+					.getSaves() - 1);
+			showSnackBar("Removed Post From Important Posts List");
+		} else {
+			postListSinglePostObject.setIs_saved(true);
+			postListSinglePostObject.setSaves(postListSinglePostObject
+					.getSaves() + 1);
+			showSnackBar("Marked Post As important");
+		}
+		markPostImportantRequestServer();
+		setInitialDataUsingnIntentObj();
+	}
+
+	private void reverseSavePostAsImportant() {
+		if (postListSinglePostObject.isIs_saved()) {
+			postListSinglePostObject.setIs_saved(false);
+			postListSinglePostObject.setSaves(postListSinglePostObject
+					.getSaves() - 1);
+		} else {
+			postListSinglePostObject.setIs_saved(true);
+			postListSinglePostObject.setSaves(postListSinglePostObject
+					.getSaves() + 1);
+		}
+		setInitialDataUsingnIntentObj();
+	}
+
+	public void upvotePostRequest(final boolean isUpvoteClicked) {
+		StringRequest req = new StringRequest(Method.POST, upvotePost,
+				new Listener<String>() {
+
+					@Override
+					public void onResponse(String arg0) {
+
+					}
+				}, new ErrorListener() {
+
+					@Override
+					public void onErrorResponse(VolleyError arg0) {
+						showSnackBar("Some error occured. Check internet connection");
+						reverseUpvotePost();
+					}
+				}) {
+			@Override
+			protected Map<String, String> getParams() throws AuthFailureError {
+				HashMap<String, String> p = new HashMap<>();
+				p.put("user_id",
+						ZPreferences.getUserProfileID(PostDetailActivity.this));
+				p.put("post_id", postListSinglePostObject.getId() + "");
+				p.put("is_upvote_clicked", Boolean.toString(isUpvoteClicked));
+				return p;
+			}
+		};
+		ZApplication.getInstance().addToRequestQueue(req, markPostAsImportant);
+	}
+
+	protected void reverseUpvotePost() {
+
+	}
+
+	@Override
+	public void onBackPressed() {
+		Fragment fragmentUserProfile = getSupportFragmentManager()
+				.findFragmentByTag(
+						Z_USER_PROFILE_VIEWED_BY_OTHER_BACKSTACK_ENTRY_TAG);
+		Fragment fragmentComments = getSupportFragmentManager()
+				.findFragmentByTag(Z_COMMENT_FRAGMENT_BACKSTACK_ENTRY_TAG);
+
+		if (fragmentUserProfile != null
+				&& !((UserProfileViewedByOtherFragment) fragmentUserProfile).fragmentDestroyed) {
+			((UserProfileViewedByOtherFragment) fragmentUserProfile)
+					.dismissScrollViewDownCalledFromActivityBackPressed();
+		} else if (fragmentComments != null) {
+			CommentsFragment frg = (CommentsFragment) fragmentComments;
+			if (frg.shouldGoBackOnBackButtonPress())
+				super.onBackPressed();
+		} else
+			super.onBackPressed();
+	}
+
+	public void switchToSeenByPeopleFragment(int postid) {
+		Bundle bundle = new Bundle();
+		bundle.putInt("postid", postid);
+
+		getSupportFragmentManager()
+				.beginTransaction()
+				.replace(R.id.fragmentcontainer,
+						SeenByPeopleFragment.newInstance(bundle))
+				.addToBackStack("tag").commit();
+	}
+
+	public void switchToCommentsFragment(int postid) {
+		Bundle bundle = new Bundle();
+		bundle.putInt("postid", postid);
+
+		getSupportFragmentManager()
+				.beginTransaction()
+				.replace(R.id.fragmentcontainer,
+						CommentsFragment.newInstance(bundle),
+						Z_COMMENT_FRAGMENT_BACKSTACK_ENTRY_TAG)
+				.addToBackStack("Z_COMMENT_FRAGMENT_BACKSTACK_ENTRY_TAG")
+				.commit();
+	}
+
+	public void switchToUserProfileViewedByOtherFragment(int userid,
+			String name, String image) {
+		Bundle bundle = new Bundle();
+		bundle.putString("name", name);
+		bundle.putInt("userid", userid);
+		bundle.putString("image", image);
+
+		getSupportFragmentManager()
+				.beginTransaction()
+				.replace(R.id.fragmentcontainer,
+						UserProfileViewedByOtherFragment.newInstance(bundle),
+						Z_USER_PROFILE_VIEWED_BY_OTHER_BACKSTACK_ENTRY_TAG)
+				.addToBackStack(
+						Z_USER_PROFILE_VIEWED_BY_OTHER_BACKSTACK_ENTRY_TAG)
+				.commit();
+	}
+
+	public void markPostImportantRequestServer() {
+		StringRequest req = new StringRequest(Method.POST, markPostAsImportant,
+				new Listener<String>() {
+
+					@Override
+					public void onResponse(String arg0) {
+
+					}
+				}, new ErrorListener() {
+
+					@Override
+					public void onErrorResponse(VolleyError arg0) {
+						showSnackBar("Some error occured. Check internet connection");
+						reverseSavePostAsImportant();
+					}
+				}) {
+			@Override
+			protected Map<String, String> getParams() throws AuthFailureError {
+				HashMap<String, String> p = new HashMap<>();
+				p.put("user_id",
+						ZPreferences.getUserProfileID(PostDetailActivity.this));
+				p.put("post_id", postListSinglePostObject.getId() + "");
+				return p;
+			}
+		};
+		ZApplication.getInstance().addToRequestQueue(req, markPostAsImportant);
+	}
 }
